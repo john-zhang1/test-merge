f895db8bed23536bbd0c71936e9608a6f3995ec0
John Zhang <john-zhang1@hotmail.com>
2016-02-28 15:54:30 -0600

    load all data

--- dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/discovery/CollectionAllSubmissions.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/discovery/CollectionAllSubmissions.java
@@ -16,13 +16,26 @@ import org.dspace.app.xmlui.wing.element.ReferenceSet;
 import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.Collection;
 import org.dspace.content.DSpaceObject;
+import org.dspace.content.Item;
+import org.dspace.content.Metadatum;
+import org.dspace.discovery.DiscoverResult;
 import org.xml.sax.SAXException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * Renders a list of recently submitted items for the collection by using discovery
+ * Renders a list of recently submitted items for the collection by using
+ * discovery
  *
  * @author Kevin Van de Velde (kevin at atmire dot com)
  * @author Mark Diggory (markd at atmire dot com)
@@ -30,9 +43,8 @@ import java.sql.SQLException;
  */
 public class CollectionAllSubmissions extends AbstractRecentSubmissionTransformer {
 
-    private static final Message T_head_recent_submissions =
-            message("xmlui.ArtifactBrowser.CollectionViewer.head_recent_submissions");
-
+    private static final Message T_head_recent_submissions
+            = message("xmlui.ArtifactBrowser.CollectionViewer.head_recent_submissions");
 
     /**
      * Displays the recent submissions for this collection
@@ -43,38 +55,112 @@ public class CollectionAllSubmissions extends AbstractRecentSubmissionTransforme
         DSpaceObject dso = HandleUtil.obtainHandle(objectModel);
         // Set up the major variables
         Collection collection = (Collection) dso;
-        if(!(dso instanceof Collection))
-        {
+        if (!(dso instanceof Collection)) {
             return;
         }
-
 
         getSubmittedAllItems(collection);
 
         //Only attempt to render our result if we have one.
-        if(queryResults == null)
-        {
+        if (queryResults == null) {
             return;
         }
 
-        if(0 < queryResults.getDspaceObjects().size()){
-            // Build the collection viewer division.
-            Division home = body.addDivision("collection-home", "primary repository collection");
-
-            Division lastSubmittedDiv = home
-                    .addDivision("collection-all-submission", "secondary all-submission");
-
-            lastSubmittedDiv.setHead(T_head_recent_submissions);
-
-            ReferenceSet allSubmitted = lastSubmittedDiv.addReferenceSet(
-                    "collection-all-submitted", null,
-                    null, "all-submissions");
-
-            for (DSpaceObject resultObj : queryResults.getDspaceObjects()) {
-                if(resultObj != null){
-                    allSubmitted.addReference(resultObj);
-                }
-            }
+        if (0 < queryResults.getDspaceObjects().size()) {
+            // Build the data file
+            getMetadataValue(queryResults);
         }
     }
+
+    private void getMetadataValue(DiscoverResult queryResults) throws UnsupportedEncodingException {
+        List<String> titles = new ArrayList<String>();
+        List<String> handles = new ArrayList<String>();
+        List<String> cities = new ArrayList<String>();
+        List<String> coordinates = new ArrayList<String>();
+        String homestate = null;
+        String homecity = null;
+        List<DSpaceObject> listobjs = queryResults.getDspaceObjects();
+
+        for (int i = 0; i < listobjs.size(); i++) {
+            DSpaceObject dsobj = listobjs.get(i);
+            handles.add("/handle/" + dsobj.getHandle());
+            Metadatum[] dcvs = dsobj.getMetadata(Item.ANY, Item.ANY, Item.ANY, Item.ANY);
+            for (Metadatum dcv : dcvs) {
+                String qualifier = dcv.qualifier;
+                if (qualifier.equals("sampleid")) {
+                    titles.add(dcv.value);
+                }
+                if (qualifier.equals("homecity")) {
+                    homecity = dcv.value;
+                }
+                if (qualifier.equals("homestate")) {
+                    homestate = dcv.value;
+                }
+                if (qualifier.equals("spatial")) {
+                    coordinates.add(dcv.value);
+                }
+            }
+            cities.add(homecity + ", " + homestate.substring(0, 2));
+        }
+        String filename = "/srv/shareok/dspace/webapps/xmlui/themes/Research/scripts/points.js";
+        WriteFile(titles, handles, coordinates, cities, filename);
+    }
+
+    private void WriteFile(List<String> titlelist, List<String> urllist, List<String> spatiallist, List<String> placelist, String filename) {
+
+        try {
+            File file = new File(filename);
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file);
+
+            writer.write("var titlelist = [");
+            for(int i=0;i<titlelist.size();i++){
+                String str = titlelist.get(i);
+                writer.write("\"" + str + "\"");
+                if(i<titlelist.size()-1){
+                    writer.write(",");
+                }
+            }
+            writer.write("];");
+            writer.write(String.format("%n"));
+
+            writer.write("var spatiallist = [");
+             for(int i=0;i<spatiallist.size();i++){
+                String str = spatiallist.get(i);
+                writer.write("\"" + str + "\"");
+                if(i<spatiallist.size()-1){
+                    writer.write(",");
+                }
+            }
+            writer.write("];");
+            writer.write(String.format("%n"));
+
+            writer.write("var urllist = [");
+             for(int i=0;i<urllist.size();i++){
+                String str = urllist.get(i);
+                writer.write("\"" + str + "\"");
+                if(i<urllist.size()-1){
+                    writer.write(",");
+                }
+            }
+            writer.write("];");
+            writer.write(String.format("%n"));
+
+            writer.write("var placelist = [");
+             for(int i=0;i<placelist.size();i++){
+                String str = placelist.get(i);
+                writer.write("\"" + str + "\"");
+                if(i<placelist.size()-1){
+                    writer.write(",");
+                }
+            }
+            writer.write("];");
+            writer.write(String.format("%n"));
+
+            writer.close();
+        } catch (IOException ex) {
+            Logger.getLogger(CollectionAllSubmissions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
